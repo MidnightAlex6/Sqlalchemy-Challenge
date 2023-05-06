91ff7a6354484ff04f55425c94bd03e64d4725b2
Alex Valerio <126301312+MidnightAlex6@users.noreply.github.com>
2023-05-05 19:13:24 -0500

    updates to the file app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,121 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
 
-
 # reflect an existing database into a new model
-
+engine = create_engine("sqlite:///hawaii.sqlite")
 # reflect the tables
-
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
-
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
+
+# Define homepage and list of available routes
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start_date<br/>"
+        f"/api/v1.0/start_date/end_date"
+    )
+
+# Define precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+
+    # Convert query results to dictionary using date as the key and prcp as the value
+    precip = {date: prcp for date, prcp in precipitation}
+
+    return jsonify(precip)
+
+# Define station route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Query all stations
+    results = session.query(Station.station, Station.name).all()
+
+    # Convert query results to a list of dictionaries
+    stations = []
+    for station, name in results:
+        station_dict = {}
+        station_dict["station"] = station
+        station_dict["name"] = name
+        stations.append(station_dict)
+
+    return jsonify(stations)
+
+# Define temperature observation route
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    # Query the primary station for all tobs from the last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+
+    # Convert query results to a list
+    temps = list(np.ravel(results))
+
+    return jsonify(temps)
+
+# Define statistics route with a start date
+@app.route("/api/v1.0/<start>")
+def stats_start(start):
+    # Calculate TMIN, TAVG, and TMAX for all dates greater than or equal to the start date
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    # Convert query results to a list of dictionaries
+    stats = []
+    for min, avg, max in results:
+        stat_dict = {}
+        stat_dict["min"] = min
+        stat_dict["avg"] = avg
+        stat_dict["max"] = max
+
+# Append each dictionary to the stats list
+    stats.append(stat_dict)
+
+# Return the JSON representation of the stats list
+return jsonify(stats)
+
 #################################################
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
